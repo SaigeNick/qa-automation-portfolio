2fafb6b3024bb5b85ee964f3c6deb9a9d2b182e0
SaigeNick <danya1102003@gmail.com>
2025-12-28 13:44:08 +0100

    Security Fix

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,11 @@
 from pages.login_page import LoginPage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def test_login_page(driver):
     login_page = LoginPage(driver)
     login_page.open()
-    login_page.login("standard_user", "secret_sauce")
+    login_page.login(os.getenv("USER_LOGIN"), os.getenv("USER_PASSWORD"))
     assert "inventory" in driver.current_url, "Ми не перейшли на сторінку інвентаря!"
